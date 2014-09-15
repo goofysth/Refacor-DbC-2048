582daccdc351a6113bb3ebef1af77e8a52cc2883
Dragan Basta <bastadragan@gmail.com>
2014-09-16 00:43:43 +0200

    Final project from Mac Book Air

---
+++ Contracts/M2GridContract.h
@@ -0,0 +1,93 @@
+//
+//  M2GridContract.h
+//  m2048
+//
+//  Created by Dragan Basta on 9/15/14.
+//  Copyright (c) 2014 Danqing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NSObjCategory.h"
+#import "Contracts.h"
+#import "M2Grid.h"
+
+#ifndef __M2GRID__
+
+#define __M2GRID__
+
+BEGIN_CONTRACT (M2Grid)
+
+        INVARIANTS (
+            FACT (self.dimension > 0);
+            FACT (self.scene != nil);
+        )
++ (void) allocCellsOnGrid:(NSMutableArray *)_grid withSize:(NSInteger)dimension VERIFY_PROC (
+                                                                                             
+        REQUIRE(
+                FACT(_grid != nil)
+                FACT(dimension >0)
+            )
+        int gridSize = [_grid count];
+        MODIFY(gridSize)
+        [super allocCellsOnGrid:_grid withSize:dimension];
+        gridSize = [_grid count];
+        ENSURE(
+                FACT((int)OLD(gridSize) + dimension == gridSize)
+              )
+)
+- (void)forEach:(IteratorBlock)block reverseOrder:(BOOL)reverse VERIFY_PROC (
+                                                                                             
+     REQUIRE(
+             FACT(self.dimension >0)
+             )
+     [super forEach:block reverseOrder:reverse];
+     
+)
+- (M2Cell *)cellAtPosition:(M2Position)position VERIFY (
+     
+     REQUIRE(
+             FACT(position.x >= 0)
+             FACT(position.x < self.dimension)
+             FACT(position.y >= 0)
+             FACT(position.y < self.dimension)
+             )
+     M2Cell *Result = [super cellAtPosition:position];
+                                                        
+     ENSURE(
+            FACT(Result != nil)
+            )
+)
+
+- (M2Tile *)tileAtPosition:(M2Position)position VERIFY (
+       REQUIRE(
+            FACT(position.x >= 0)
+            FACT(position.x < self.dimension)
+            FACT(position.y >= 0)
+            FACT(position.y < self.dimension)
+       )
+        M2Tile *Result = [super cellAtPosition:position];
+                                                        
+        ENSURE(
+           FACT(Result != nil)
+        )
+)
+- (BOOL)hasAvailableCells; VERIFY(
+REQUIRE(
+        FACT(self.dimension > 0)
+        )
+      BOOL Result = [super hasAvailableCells];
+)
+
+- (void)insertTileAtRandomAvailablePositionWithDelay:(BOOL)delay VERIFY_PROC (
+    REQUIRE(FACT(self.scene != nil))
+      M2Scene *theScene = self.scene;
+      
+  [super insertTileAtRandomAvailablePositionWithDelay:delay];
+      M2Scene *newScene = self.scene;
+      ENSURE(
+             FACT(theScene == newScene);
+             )
+)
+END_CONTRACT
+
+#endif
---
+++ Contracts/M2SettingsDetailViewControllerContract.h
@@ -0,0 +1,82 @@
+//
+//  M2SettingsDetailViewControllerContract.h
+//  m2048
+//
+//  Created by Dragan Basta on 9/15/14.
+//  Copyright (c) 2014 Danqing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NSObjCategory.h"
+#import "Contracts.h"
+#import "../m2048/Controller/M2SettingsDetailViewController.h"
+
+#ifndef __M2SETTINGSDETAILVIEWCONTROLLER__
+
+#define __M2SETTINGSDETAILVIEWCONTROLLER__
+
+BEGIN_CONTRACT (M2SettingsDetailViewController)
+
+    INVARIANTS (
+        FACT (self.options != nil);
+        FACT (self.footer != nil);
+    )
+
+    - (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView VERIFY (
+            REQUIRE (
+                FACT (tableView != nil);
+            )
+            int Result = [super numberOfSectionsInTableView:tableView]; // call the method
+            ENSURE (
+               FACT (Result > 0);
+            )
+        )
+
+    - (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section VERIFY (
+       REQUIRE (
+                FACT (tableView != nil);
+                )
+       int Result = [super tableView:tableView numberOfRowsInSection:section]; // call the method
+       ENSURE (
+               FACT (Result > 0);
+               )
+       )
+
+    - (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section VERIFY (
+        REQUIRE (
+                 FACT (tableView != nil);
+                 )
+        NSString *footer = self.footer;
+        MODIFY(footer)
+        NSString *Result = [super tableView:tableView titleForFooterInSection:section]; // call the method
+        ENSURE (
+                FACT (Result != nil);
+                FACT (self.footer == OLD(footer))
+                )
+        )
+
+    - (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath VERIFY (
+        REQUIRE (
+                 FACT (tableView != nil);
+                 FACT (indexPath != nil)
+                 )
+        UITableViewCell *Result = [super tableView:tableView cellForRowAtIndexPath:indexPath]; // call the method
+        ENSURE (
+                FACT (Result != nil);
+                FACT (Result.textLabel.text !=nil)
+                FACT (Result.tintColor != nil)
+                )
+        )
+    - (void) tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath VERIFY_PROC (
+        REQUIRE(
+                FACT(tableView != nil);
+                FACT(indexPath != nil);
+                )
+        [super tableView:tableView didDeselectRowAtIndexPath:indexPath];ENSURE(
+                FACT(GSTATE.needRefresh != YES);
+            )
+        )
+
+END_CONTRACT
+
+#endif
